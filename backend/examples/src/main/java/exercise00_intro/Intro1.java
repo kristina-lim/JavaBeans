7edf78e0d44bbd9d43e1c2163f33e357b00a21be
Andy Bainton <60830258+baintonaj@users.noreply.github.com>
2025-09-09 12:03:32 -0700

    Intial backend structure

---
+++ backend/examples/src/main/java/exercise00_intro/Intro1.java
@@ -0,0 +1,13 @@
+package exercise00_intro;
+
+public class Intro1 {
+    public static void main(String[] args) {
+        System.out.println("       Welcome to...");
+        System.out.println("  __    _  __      __   _");
+        System.out.println("  __   _ _  __    __   _ _");
+        System.out.println("_ __  __ __  __  __   __  __");
+        System.out.println("____ __   __   __    __    __ ");
+        System.out.println("/** ***** B E A N S ******** */");
+
+    }
+}
