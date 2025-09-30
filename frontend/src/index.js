0d756c03e1174b645cf9e6e5fb26ff212d70cff3
kristina-lim <kristinahlim@gmail.com>
2025-09-30 13:10:11 -0700

    Fix bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App/App';
 import reportWebVitals from './reportWebVitals';
+import { TerminalContextProvider } from 'react-terminal'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TerminalContextProvider>
+        <App />
+    </TerminalContextProvider>
   </React.StrictMode>
 );
 
---
+++ frontend/src/pages/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from '../../components/NavBar/NavBar';
+import Terminal from '../../components/Terminal/Terminal';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+        <Terminal />
+    </div>
+  );
+}
+
+export default App;
