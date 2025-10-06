97676a0a5edf67fc59f66147a2fc41ac130afb67
kristina-lim <kristinahlim@gmail.com>
2025-10-06 10:37:10 -0700

    Add javabeans logo

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,13 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../logo.png';
 
 export default function NavBar() {
     return(
         <>
             <Navbar className="d-flex justify-content-start">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            width="40"
+                            height="40"
+                        />
+                    </Navbar.Brand>
                     <Nav.Link href="#home">JavaBeans</Nav.Link>
                     <Nav.Link href="#about">About</Nav.Link>
                     <Nav.Link href="#faq">FAQ</Nav.Link>
