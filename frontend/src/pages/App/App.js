1f75212007c8c80656924ff68cf143c46337a5e6
kristina-lim <kristinahlim@gmail.com>
2025-10-06 19:26:56 -0700

    Make a terminals page, split terminals component into two so its more readable, adjust imports

--- frontend/src/pages/App/App.js
+++ frontend/src/pages/App/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
-import MicroTerminal from '../../components/Terminals/MicroTerminal';
 import Home from '../Home/Home';
 import About from '../About/About';
 import Faq from '../Faq/Faq';
