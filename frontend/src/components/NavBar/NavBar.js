b860cb41f1b8b680737e6e7ffe8e9fd79ceb1b58
kristina-lim <kristinahlim@gmail.com>
2025-10-06 18:09:09 -0700

    Add routers

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,16 +9,16 @@ export default function NavBar() {
         <>
             <Navbar className="d-flex justify-content-start">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand as={Link} to="/">
                         <img
                             src={logo}
                             width="40"
                             height="40"
                         />
                     </Navbar.Brand>
-                    <Nav.Link href="#home">JavaBeans</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#faq">FAQ</Nav.Link>
+                    <Nav.Link as={Link} to="/">JavaBeans</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
                 </Container>
             </Navbar>
         </>
---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,11 @@
+//import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+        <h1>Welcome to JavaBeans!</h1>
+        <p>This is a Rustlings inspired app. Two students who are pursuing their master's degree in computer science, and also in the process of learning java.</p>
+        <p>JavaBeans is a fun, hands-on way to learn Java through interactive exercises.</p>
+	</div>
+  );
+}
