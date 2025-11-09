5b4fc525e318018e1ede091ee47a1b0ae319ac6f
kristina-lim <kristinahlim@gmail.com>
2025-11-09 15:20:17 -0800

    Put terminals side by side

--- frontend/src/pages/Terminals/Terminals.js
+++ frontend/src/pages/Terminals/Terminals.js
@@ -2,8 +2,18 @@ import MicroTerminal from '../../components/MicroTerminal/MicroTerminal';
 import TestTerminal from '../../components/TestTerminal/TestTerminal';
 
 export default function Terminals() {
+  const styles =  {
+      container: {
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '20px',
+          padding: '20px',
+          height: '100vh',
+      }
+    };
+
   return (
-    <div>
+    <div style={styles.container}>
       <h2>Micro Terminal</h2>
       <MicroTerminal />
       <h2>Testing Terminal</h2>
